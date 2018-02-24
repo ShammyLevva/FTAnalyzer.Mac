15c4e7c8066390eae5d1219895e6eebc3c6575f6
Deryck Brown <deryckfb@sky.com>
2018-02-24 18:33:24 +0000

    Working on opening GEDCOM file. File loads but with no UI.

---
+++ FTAnalyzer.Mac/GedcomDocument.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace FTAnalyzer.Mac
+{
+    [Register("GedcomDocument")]
+    public class GedcomDocument : NSDocument
+    {
+        FamilyTree _familyTree = FamilyTree.Instance;
+
+        public GedcomDocument()
+        {
+        }
+
+        public override bool ReadFromUrl(NSUrl url, string typeName, out NSError outError)
+        {
+            var textProgress = new Progress<string>(m => { });
+            var document = _familyTree.LoadTreeHeader(url.Path, textProgress);
+            if (document ==  null) {
+                outError = null;
+                return false;
+            }
+
+            var sourceProgress = new Progress<int>(s => { });
+            _familyTree.LoadTreeSources(document, sourceProgress, textProgress);
+            _familyTree.LoadTreeIndividuals(document, sourceProgress, textProgress);
+            _familyTree.LoadTreeFamilies(document, sourceProgress, textProgress);
+            _familyTree.LoadTreeRelationships(document, sourceProgress, textProgress);
+
+            outError = null;
+            return true;
+        }
+    }
+}
