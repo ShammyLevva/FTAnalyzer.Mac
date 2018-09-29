8d87f2678adaf8e4f61210e77ca645a1c7881aeb
Deryck Brown <deryckfb@sky.com>
2018-09-29 17:27:45 +0100

    Fixed closing old file when opening a new file.

--- FTAnalyzer.Mac/AppDelegate.cs
+++ FTAnalyzer.Mac/AppDelegate.cs
@@ -4,36 +4,50 @@ using Foundation;
 
 namespace FTAnalyzer.Mac
 {
-	[Register("AppDelegate")]
-	public class AppDelegate : NSApplicationDelegate
-	{
-		public GedcomDocument Document { get; set; }
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        bool _documentOpening;
 
-		public override void DidFinishLaunching(NSNotification notification)
-		{
-			// Insert code here to initialize your application
-			var window = NSApplication.SharedApplication.Windows[0];
-			window.Title = "Family Tree Analyzer v" + Version;
-		}
+        public GedcomDocument Document { get; set; }
 
-		public override void WillTerminate(NSNotification notification)
-		{
-			// Insert code here to tear down your application
-		}
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+            var window = NSApplication.SharedApplication.Windows[0];
+            window.Title = "Family Tree Analyzer v" + Version;
+        }
 
-		public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
-		{
-			return true;
-		}
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
 
-		string Version
-		{
-			get
-			{
-				var version = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString();
-				var build = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion").ToString();
-				return version + "." + build;
-			}
-		}
-	}
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
+        {
+            return !_documentOpening;
+        }
+
+        public override bool OpenFile(NSApplication sender, string filename)
+        {
+            if (Document != null)
+            {
+                _documentOpening = true;
+                Document.Close();
+                _documentOpening = false;
+            }
+
+            return false;
+        }
+
+        string Version
+        {
+            get
+            {
+                var version = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString();
+                var build = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion").ToString();
+                return version + "." + build;
+            }
+        }
+    }
 }
--- FTAnalyzer.Mac/GedcomDocument.cs
+++ FTAnalyzer.Mac/GedcomDocument.cs
@@ -8,71 +8,87 @@ namespace FTAnalyzer.Mac
     public class GedcomDocument : NSDocument
     {
         readonly FamilyTree _familyTree = FamilyTree.Instance;
-       
+
         [Export("canConcurrentlyReadDocumentsOfType:")]
         public static new bool CanConcurrentlyReadDocumentsOfType(string fileType) => true;
 
-		public override bool ReadFromUrl(NSUrl url, string typeName, out NSError outError)
-		{
+        public override bool ReadFromUrl(NSUrl url, string typeName, out NSError outError)
+        {
 
-			outError = NSError.FromDomain(NSError.OsStatusErrorDomain, -4);
+            outError = NSError.FromDomain(NSError.OsStatusErrorDomain, -4);
 
-			GedcomDocumentViewController documentViewController = null;
-			BindingListViewController<IDisplayIndividual> individualsViewController = null;
-			BindingListViewController<IDisplayFamily> familiesViewController = null;
-			BindingListViewController<IDisplaySource> sourcesViewController = null;
-			BindingListViewController<IDisplayOccupation> occupationsViewController = null;
-			BindingListViewController<IDisplayFact> factsViewController = null;
-			NSTabViewController tabbedViewController = null;
+            GedcomDocumentViewController documentViewController = null;
+            BindingListViewController<IDisplayIndividual> individualsViewController = null;
+            BindingListViewController<IDisplayFamily> familiesViewController = null;
+            BindingListViewController<IDisplaySource> sourcesViewController = null;
+            BindingListViewController<IDisplayOccupation> occupationsViewController = null;
+            BindingListViewController<IDisplayFact> factsViewController = null;
+            NSTabViewController tabbedViewController = null;
 
-			InvokeOnMainThread(() =>
-			{
+            InvokeOnMainThread(() =>
+            {
                 var window = NSApplication.SharedApplication.MainWindow;
-                
-				tabbedViewController = window.ContentViewController as NSTabViewController;
 
-				documentViewController = tabbedViewController.ChildViewControllers[0] as GedcomDocumentViewController;
-				documentViewController.ClearAllProgress();
+                tabbedViewController = window.ContentViewController as NSTabViewController;
 
-				var mainListsViewController = tabbedViewController.ChildViewControllers[1] as NSTabViewController;
+                //Make sure the loading tab is seleceted.
+                tabbedViewController.SelectedTabViewItemIndex = 0;
 
-				individualsViewController = new BindingListViewController<IDisplayIndividual>("Individuals");
-				familiesViewController = new BindingListViewController<IDisplayFamily>("Families");
-				sourcesViewController = new BindingListViewController<IDisplaySource>("Sources");
-				occupationsViewController = new BindingListViewController<IDisplayOccupation>("Occupations");
-				factsViewController = new BindingListViewController<IDisplayFact>("Facts");
+                var mainListsViewController = tabbedViewController.ChildViewControllers[1] as NSTabViewController;
 
-				mainListsViewController.AddChildViewController(individualsViewController);
-				mainListsViewController.AddChildViewController(familiesViewController);
-				mainListsViewController.AddChildViewController(sourcesViewController);
-				mainListsViewController.AddChildViewController(occupationsViewController);
-				mainListsViewController.AddChildViewController(factsViewController);
-			});
+                // Remove any existing lists from a previous document.
+                while (mainListsViewController.ChildViewControllers.Length > 0)
+                {
+                    mainListsViewController.RemoveChildViewController(0);
+                }
 
-			var document = _familyTree.LoadTreeHeader(url.Path, documentViewController.Messages);
-			if (document == null)
-			{
-				documentViewController.Messages.Report("\n\nUnable to load file " + url.Path + "\n");
-				return false;
-			}
+                documentViewController = tabbedViewController.ChildViewControllers[0] as GedcomDocumentViewController;
+                documentViewController.ClearAllProgress();
 
-			_familyTree.LoadTreeSources(document, documentViewController.Sources, documentViewController.Messages);
-			_familyTree.LoadTreeIndividuals(document, documentViewController.Individuals, documentViewController.Messages);
-			_familyTree.LoadTreeFamilies(document, documentViewController.Families, documentViewController.Messages);
-			_familyTree.LoadTreeRelationships(document, documentViewController.Relationships, documentViewController.Messages);
+                individualsViewController = new BindingListViewController<IDisplayIndividual>("Individuals");
+                familiesViewController = new BindingListViewController<IDisplayFamily>("Families");
+                sourcesViewController = new BindingListViewController<IDisplaySource>("Sources");
+                occupationsViewController = new BindingListViewController<IDisplayOccupation>("Occupations");
+                factsViewController = new BindingListViewController<IDisplayFact>("Facts");
 
-			individualsViewController.RefreshDocumentView(_familyTree.AllDisplayIndividuals);
-			familiesViewController.RefreshDocumentView(_familyTree.AllDisplayFamilies);
-			sourcesViewController.RefreshDocumentView(_familyTree.AllDisplaySources);
-			occupationsViewController.RefreshDocumentView(_familyTree.AllDisplayOccupations);
-			factsViewController.RefreshDocumentView(_familyTree.AllDisplayFacts);
+                mainListsViewController.AddChildViewController(individualsViewController);
+                mainListsViewController.AddChildViewController(familiesViewController);
+                mainListsViewController.AddChildViewController(sourcesViewController);
+                mainListsViewController.AddChildViewController(occupationsViewController);
+                mainListsViewController.AddChildViewController(factsViewController);
+            });
 
-			documentViewController.Messages.Report("\n\nFinished loading file " + url.Path + "\n");
-			RaiseDocumentModified(this);
-			return true;
-		}
+            var document = _familyTree.LoadTreeHeader(url.Path, documentViewController.Messages);
+            if (document == null)
+            {
+                documentViewController.Messages.Report("\n\nUnable to load file " + url.Path + "\n");
+                return false;
+            }
 
-		#region Events
+            _familyTree.LoadTreeSources(document, documentViewController.Sources, documentViewController.Messages);
+            _familyTree.LoadTreeIndividuals(document, documentViewController.Individuals, documentViewController.Messages);
+            _familyTree.LoadTreeFamilies(document, documentViewController.Families, documentViewController.Messages);
+            _familyTree.LoadTreeRelationships(document, documentViewController.Relationships, documentViewController.Messages);
+
+            individualsViewController.RefreshDocumentView(_familyTree.AllDisplayIndividuals);
+            familiesViewController.RefreshDocumentView(_familyTree.AllDisplayFamilies);
+            sourcesViewController.RefreshDocumentView(_familyTree.AllDisplaySources);
+            occupationsViewController.RefreshDocumentView(_familyTree.AllDisplayOccupations);
+            factsViewController.RefreshDocumentView(_familyTree.AllDisplayFacts);
+
+            documentViewController.Messages.Report("\n\nFinished loading file " + url.Path + "\n");
+
+            InvokeOnMainThread(() =>
+            {
+                var app = (AppDelegate)NSApplication.SharedApplication.Delegate;
+                app.Document = this;
+            });
+
+            RaiseDocumentModified(this);
+            return true;
+        }
+
+        #region Events
         public delegate void DocumentModifiedDelegate(GedcomDocument document);
         public event DocumentModifiedDelegate DocumentModified;
 
