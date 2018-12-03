0766b97adc897ff716831741ec263811dbef1ebe
Alexander Bisset (Mac) <ambisset@btinternet.com>
2018-12-03 23:35:56 +0000

    add new files

---
+++ FTAnalyzer/Utilities/ReplaceViewSegue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace FTAnalyzer.Utilities
+{
+    [Register("ReplaceViewSeque")]
+    public class ReplaceViewSeque : NSStoryboardSegue
+    {
+        #region Constructors
+        public ReplaceViewSeque()
+        {
+        }
+
+        public ReplaceViewSeque(string identifier, NSObject sourceController, NSObject destinationController) : base(identifier, sourceController, destinationController)
+        {
+        }
+
+        public ReplaceViewSeque(IntPtr handle) : base(handle)
+        {
+        }
+
+        public ReplaceViewSeque(NSObjectFlag x) : base(x)
+        {
+        }
+        #endregion
+
+        #region Override Methods
+        public override void Perform()
+        {
+            // Cast the source and destination controllers
+            var destination = DestinationController as NSViewController;
+
+            // Is there a source?
+            if (!(SourceController is NSViewController source))
+            {
+                // No, get the current key window
+                var window = NSApplication.SharedApplication.KeyWindow;
+                window.ContentViewController = destination; // Swap the controllers
+                window.ContentViewController?.RemoveFromParentViewController(); // Release memory
+            }
+            else
+            {
+                source.View.Window.ContentViewController = destination; // Swap the controllers
+                source.RemoveFromParentViewController(); // Release memory
+            }
+        }
+        #endregion
+
+    }
+}
