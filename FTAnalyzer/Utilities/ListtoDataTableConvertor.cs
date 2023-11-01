6ab650141e338862b8efbc17bec73518a03ca73f
Karl <karl1c@hotmail.com>
2023-11-01 21:23:41 +0000

    Added (commented out) code to ListtoDataTableConvertor.cs from FTAnalyzer commit 223610f

--- FTAnalyzer/Utilities/ListtoDataTableConvertor.cs
+++ FTAnalyzer/Utilities/ListtoDataTableConvertor.cs
@@ -26,5 +26,21 @@ namespace FTAnalyzer.Utilities
             //put a breakpoint here and check datatable
             return dataTable;
         }
+
+        //public DataTable ToDataTable<T>(this IList<T> data)
+        //{
+        //    PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+        //    DataTable table = new DataTable();
+        //    foreach (PropertyDescriptor prop in properties)
+        //        table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+        //    foreach (T item in data)
+        //    {
+        //        DataRow row = table.NewRow();
+        //        foreach (PropertyDescriptor prop in properties)
+        //            row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+        //        table.Rows.Add(row);
+        //    }
+        //    return table;
+        //}
     }
 }
