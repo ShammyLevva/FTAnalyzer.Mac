041b0715868d9dd7d95068ef754d82c61f994cc7
Karl <karl1c@hotmail.com>
2023-11-03 20:03:44 +0000

    Added SetupHttpClient to Main.cs as per FTAnalyzer commit 0fada1f

--- FTAnalyzer/Main.cs
+++ FTAnalyzer/Main.cs
@@ -1,15 +1,29 @@
-﻿using FTAnalyzer.Exports;
+﻿using System.Net;
+using FTAnalyzer.Exports;
 
 namespace FTAnalyzer
 {
     static class MainClass
     {
-        public static readonly HttpClient Client = new();
+        public static HttpClient Client { get; private set; }
+        public static CookieContainer Cookies { get; private set; }
         public static readonly LostCousinsClient LCClient = new();
+
         static void Main(string[] args)
         {
             NSApplication.Init();
+            SetupHttpClient();
             NSApplication.Main(args);
+        }
+
+        static void SetupHttpClient()
+        {
+            Cookies = new();
+            HttpClientHandler handler = new()
+            {
+                CookieContainer = Cookies
+            };
+            Client = new(handler);
         }
     }
 }
