bd3ee86084f7197ccf82a1a211a175a9aeccb9ed
Karl <karl1c@hotmail.com>
2023-11-03 20:09:24 +0000

    Removed SetupHttpClient as per FTAnalyzer commit 9720bdf

--- FTAnalyzer/Main.cs
+++ FTAnalyzer/Main.cs
@@ -5,25 +5,13 @@ namespace FTAnalyzer
 {
     static class MainClass
     {
-        public static HttpClient Client { get; private set; }
-        public static CookieContainer Cookies { get; private set; }
-        public static readonly LostCousinsClient LCClient = new();
+        public static HttpClient Client = new();
+        public static LostCousinsClient LCClient = new();
 
         static void Main(string[] args)
         {
             NSApplication.Init();
-            SetupHttpClient();
             NSApplication.Main(args);
-        }
-
-        static void SetupHttpClient()
-        {
-            Cookies = new();
-            HttpClientHandler handler = new()
-            {
-                CookieContainer = Cookies
-            };
-            Client = new(handler);
         }
     }
 }
